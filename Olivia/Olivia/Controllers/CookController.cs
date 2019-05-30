571a3da46ffd70404a74892a0b173f2ed61842a0
Diogo Sobral <diogosobral98@gmail.com>
2019-05-30 19:03:52 +0100

    maps implementados

---
+++ Olivia/Olivia/Controllers/CookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Olivia.Models;
+using Olivia.DataAccess;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Olivia.Controllers
+{
+    public class CookController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "User");
+        } 
+
+        public IActionResult Shopping(int id)
+        {
+            RecipeDAO dao = new RecipeDAO();
+            Recipe recipe = dao.FindById(id);
+            return View(recipe);
+        }
+    }
+}
